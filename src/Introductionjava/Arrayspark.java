54e69d549c23538c9f9aacaa7bdc6fc602146708
yashpancholi04 <yashpancholi04@gmail.com>
2026-03-20 22:59:09 +0530

    just array basic

---
+++ src/Introductionjava/Arrayspark.java
@@ -0,0 +1,12 @@
+package Introductionjava;
+
+public class Arrayspark {
+    public static void main(String args []){
+        int [] num = new int[5];
+        for(int i=0;i<num.length;i++){
+            num[i] = i;
+        }
+        System.out.println(num[3]);
+
+    }
+}
